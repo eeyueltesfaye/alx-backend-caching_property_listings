9a1cf46448daf21f918b5789ce79a9ff77578a51
Eyuel Tesfaye <163458107+eeyueltesfaye@users.noreply.github.com>
2025-07-13 23:08:17 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page  
 
 from properties.utils import get_all_properties
-
 
 # @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
